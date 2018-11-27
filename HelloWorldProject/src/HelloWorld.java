c98994711b7d0da85ab7d58639a386ed151b4ffa
StreamFreak <45207602+StreamFreak@users.noreply.github.com>
2018-11-27 17:54:02 +0100

    Update HelloWorld.java

--- HelloWorldProject/src/HelloWorld.java
+++ HelloWorldProject/src/HelloWorld.java
@@ -3,8 +3,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hallo Klasse");
-		// das ist ein Kommentar, kein code der ausgeführt wird.
+		System.out.println("Hallo Fritz");
+		// das ist ein Kommentar, kein code der ausgefÃ¼hrt wird.
 		// das ist ein einzeiliger Kommentar
 		/*
 		 * Mehrzeiligen Schreiben Kommentar

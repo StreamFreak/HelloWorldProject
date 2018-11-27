45d2131d60dacc5db8d177ac6380ffaae8cebbdc
lukas.hofbauer <lukas.hofbauer@kk-gym-edv1-13.gym.kk.local>
2018-11-27 17:41:57 +0100

    kommentare hinzugefügt

--- HelloWorldProject/src/HelloWorld.java
+++ HelloWorldProject/src/HelloWorld.java
@@ -3,7 +3,13 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       System.out.println("Hallo Klasse");
+		System.out.println("Hallo Klasse");
+		// das ist ein Kommentar, kein code der ausgeführt wird.
+		// das ist ein einzeiliger Kommentar
+		/*
+		 * Mehrzeiligen Schreiben Kommentar
+		 */
+
 	}
 
 }

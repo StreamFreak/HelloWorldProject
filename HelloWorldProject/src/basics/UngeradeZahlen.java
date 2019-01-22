c937d5c149fdb21b7cefe52c8d8db818a064aeae
lukas.hofbauer <lukas.hofbauer@kk-gym-edv1-13.gym.kk.local>
2019-01-22 18:05:51 +0100

    BubbleSort & UngeradeZahlen - neue Version! bei BubbleSort weitermachen nächstes mal!

---
+++ HelloWorldProject/src/basics/UngeradeZahlen.java
@@ -0,0 +1,16 @@
+package basics;
+
+public class UngeradeZahlen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < 100; ++i) {
+		 if (i%2 == 1) {
+		    System.out.println("Die zahl ist" + " " + i + " " + "=> ungerade");
+		
+		}
+	}
+
+	}
+
+}

a8da4c032bd0e7c7bade46f5549e43bcdff97156
lukas.hofbauer <lukas.hofbauer@kk-gym-edv1-13.gym.kk.local>
2019-02-19 17:54:59 +0100

    Bubble Sort

--- HelloWorldProject/src/sortieren/BubbleSort.java
+++ HelloWorldProject/src/sortieren/BubbleSort.java
@@ -6,15 +6,17 @@ public class BubbleSort {
 		// TODO Auto-generated method stub
 		int[] zahlen = { 3, 1, 9, 5, 2, 8, 6, 5 };
 		// 
-		System.out.println(zahlen[0]); 
-		System.out.println(zahlen[1]); 
-		// 
-		System.out.println(zahlen.length);
-		//
-		for (int i=0; i < zahlen.length; i++)
-		{
-		   System.out.println(zahlen[i]);
-		   
-		}
+		for (int n=zahlen.length; n>1; --n) {
+			for (int i=0; i<n-1; ++i ) {
+			  if (zahlen[i] > zahlen[i+1]) {
+				  int help = zahlen[i];
+				  zahlen [i] = zahlen [i+1];
+				  zahlen [i+1] = help;
+			  } // Ende if
+		   } // Ende innere for-Schleife
+		} // Ende äußere for-Schleife
+	   //
+		for (int i : zahlen)
+			System.out.println(i);
 	}
 }
